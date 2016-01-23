cfd45123d1c74e8ef7ff06998c6c741ae7da1e93
Tom Kowalski <kowalski.tom0@gmail.com>
2016-01-23 07:15:38 -0500

    Add Promises to indico api calls

--- twitterAPI.js
+++ twitterAPI.js
@@ -14,27 +14,65 @@ module.exports = {
 		client.get('statuses/user_timeline', {screen_name: name, count:3000, trim_user:true}, function(error, tweets, response){
   			if(error) throw error;
   			var strOut = "";
-			
-			//var data = JSON.parse(tweets);
-			//console.log(data);
 			for(var i = 0; i < tweets.length; i++) {
 				strOut = strOut + tweets[i].text;
 			}
 			var indico = require('indico.io');
 			indico.apiKey = process.env.INDICO_KEY;
-			indico.personas(strOut)
-  			.then(function(res) {
-    			callback.send(res);
-  			}).catch(function(err) {
-    			console.warn(err);
-  			});
-  			indico.political(strOut)
-  			.then(function(res) {
-  				callback.send(res);
-  			}).catch(function(err) {
-  				console.warn(err);
+			var data = {
+				personas: {
+					key: [],
+					values: []
+				},
+				political: {
+					key: [],
+					values: []
+				}
+			};
+			var promArr = [];
+			promArr.push(
+				indico.personas(strOut)
+  				.then(function(res) {
+  					return new Promise( function(resolve, reject) {
+  						for (var key in res) {
+  							if (res.hasOwnProperty(key)) {
+    							data.personas.key.push(key);
+  								data.personas.values.push(res[key]);
+  							}
+						}
+						resolve();
+					});
+    			}).catch(function(err) {
+    				return new Promise( function(resolve, reject) {
+           				reject();
+         			});
+  				}));
+  			promArr.push(
+  				indico.political(strOut)
+  				.then(function(res) {
+  					return new Promise( function(resolve, reject) {
+  						for (var key in res) {
+  							if (res.hasOwnProperty(key)) {
+    							data.political.key.push(key);
+  								data.political.values.push(res[key]);
+  							}
+						}
+						resolve();
+					});		
+  				}).catch(function(err) {
+    				return new Promise( function(resolve, reject) {
+           				reject();
+         			});
+  			}));
+  			Promise.all(promArr).then(
+  				function() {
+  					callback(data);
+  				},
+  				function() {
+  					console.log("error");
   			});
 		});
+		
 	}
 };
 function getTweetsText(data, callback) {
