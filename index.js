8d96f45451cff9e95279ec5e16f7b3cafcee08a8
Tom Kowalski <kowalski.tom0@gmail.com>
2016-01-23 04:42:24 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser');
+var twitter = require('./twitterAPI.js');
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded());
+
+app.get('/', function(request, response) {
+	response.send(' <form action="game" method="get"> <input type="text" name="name"/><input type="submit" /></form>');
+	
+	//console.log(twitter.getTweets());
+  	//twitter.getTweets(response);
+  	//response.send('Hello World!')
+})
+
+app.get('/game', function (req, res) {
+	console.log(req.query.name);
+    twitter.getTweets(req.query.name, res);
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
---
+++ twitterAPI.js
@@ -0,0 +1,35 @@
+module.exports = {
+	getTweets : function(name, callback) {
+		var Twitter = require('twitter');
+ 		var env = require('node-env-file');
+ 		env('.env');
+		var client = new Twitter({
+  			consumer_key: process.env.TWITTER_CONSUMER_KEY,
+		    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+		    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+		    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+		});
+		client.get('statuses/user_timeline', {screen_name: name, count:3000, trim_user:true}, function(error, tweets, response){
+  			if(error) throw error;
+  			var strOut = "";
+			
+			//var data = JSON.parse(tweets);
+			//console.log(data);
+			for(var i = 0; i < tweets.length; i++) {
+				strOut = strOut + tweets[i].text;
+			}
+			var indico = require('indico.io');
+			indico.apiKey = process.env.INDICO_KEY;
+			indico.personas(strOut)
+  			.then(function(res) {
+    			callback.send(res);
+  			}).catch(function(err) {
+    			console.warn(err);
+  			});
+		});
+	}
+};
+function getTweetsText(data, callback) {
+	
+	callback.send(arr);
+}
